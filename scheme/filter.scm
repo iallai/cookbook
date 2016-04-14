e53706aa1bd973bc265dbcf7ff8348618a991c0d
iallai <iallai.info@gmail.com>
2016-04-14 14:37:02 +0800

    filter

---
+++ scheme/filter.scm
@@ -0,0 +1,4 @@
+(define (filter predicate sequence) (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+	(filter predicate (cdr sequence)))) (else (filter predicate (cdr sequence)))))
