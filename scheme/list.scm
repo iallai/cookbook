6acb852f2516d793b423f86e39de9869ef6fb6e7
iallai <iallai.info@gmail.com>
2016-04-22 21:06:49 +0800

    Update list.scm
    fix words

--- scheme/list.scm
+++ scheme/list.scm
@@ -2,12 +2,12 @@
 "等价于"
 (cons 1 (cons 2 (cons 3 nil)))
 
-(defien (list-ref item n)
+(define (list-ref item n)
 	(if(= n 0)
 		(car item)
 		(list-ref cdr(item) (- n 1))))
 "通过递归"
-(defien (list-length items)
+(define (list-length items)
 	(if (null? items)
 		0
 		(+ 1 (list-length (cdr items)))))
